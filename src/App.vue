85dccd9b20281534e79505f878e870618912cf8e
roogeer <1681991@qq.com>
2019-12-19 13:15:11 +0800

    sudoku v0.1 20191219 13:15

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<!--<img alt="Vue logo" src="./assets/logo.png" />-->
+		<HelloWorld msg="Sudoku v0.1" />
+		<!-- <button @click="test">查看B1单元格数据</button> -->
+		<div class="game">
+			<div class="sudoku">
+				<template v-for="(area_row, index_area_row) in 3">
+					<template v-for="(area_col, index_area_col) in 3">
+						<div class="sudoku_area" :key="(index_area_row - 1) * 3 + (index_area_col -1 )">
+							<template v-for="row in 3">
+								<template v-for="col in 3">
+									<cell :initcellnumber="sudoku_data[(area_row - 1) * 27 + (area_col - 1) * 3 + (row -1) * 9 + (col - 1)]" :key="(area_row -1 ) * 27 + (area_col -1 ) * 3 + (row - 1) * 9 + (col - 1)" :id="(area_row - 1) * 27 + (area_col - 1) * 3 + (row -1) * 9 + col" />
+								</template>
+							</template>
+						</div>
+					</template>
+				</template>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import HelloWorld from "./components/HelloWorld.vue";
+	import cell from "./components/cell.vue";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: "app",
+		data: function() {
+			return {
+				sudoku_array:[
+					0,4,1,0,0,2,0,3,0,
+					6,0,0,0,8,0,0,7,0,
+					8,0,0,1,9,0,4,5,0,
+					4,0,0,5,0,6,7,0,0,
+					1,2,0,0,0,3,0,4,8,
+					0,0,3,4,0,0,0,0,1,
+					0,1,8,0,6,5,0,0,4,
+					0,5,0,0,3,0,0,0,7,
+					0,6,0,8,0,0,3,1,0
+				],
+				sudoku_data:[]
+			};
+		},
+
+		methods: {
+			initSudokuData(){
+				let _temp = [];
+				console.log(this.sudoku_array.length);
+				for(let i=0; i<this.sudoku_array.length; i++){
+					if(0===this.sudoku_array[i]){
+						_temp.push({
+							innerdata: [],
+							system: false,
+							userlocked: true,
+						});
+					}
+					else{
+						_temp.push({
+							innerdata: [this.sudoku_array[i]],
+							system: true,
+							userlocked: false,
+						})
+					}
+				}
+				console.log(_temp);
+				this.sudoku_data = _temp;
+			}
+		},
+		created:function(){
+			this.initSudokuData()
+		},
+		components: {
+			HelloWorld,
+			cell
+		}
+	};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: "Avenir", Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+
+	.game {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.sudoku {
+		display: grid;
+		grid-template-columns: repeat(3, 180px);
+		grid-template-rows: repeat(3, 180px);
+		grid-gap: 12px 12px
+	}
+
+	.sudoku_area {
+		display: grid;
+		grid-template-columns: repeat(3, 60px);
+		grid-template-rows: repeat(3, 60px);
+		grid-gap: 3px 3px;
+	}
 </style>
---
+++ src/components/cell.vue
@@ -0,0 +1,375 @@
+<template>
+	<!--是系统提供的数据-->
+	<div v-if="this.cellnumber.system" class="container01" tabindex="0" @keyup="cellkeyup($event)">
+		<div class="area0">
+			<span>{{this.cellnumber.innerdata[0]}}</span>
+		</div>
+	</div>
+	<!--用户数据，且锁定了唯一值-->
+	<div v-else-if="this.cellnumber.userlocked" class="container01" tabindex="0" @keyup="cellkeyup($event)">
+		<div class="areaUserlocked">
+			<span>{{this.cellnumber.innerdata[0]}}</span>
+		</div>
+	</div>
+	<!--用户数据，没有锁定唯一值，数据按九宫格展示-->
+	<div v-else class="container" tabindex="0" @keyup="cellkeyup($event)">
+		<div class="area1">
+			<span>{{(this.cellnumber.innerdata.indexOf(1)!==-1)?1:''}}</span>
+		</div>
+		<div class="area2">
+			<span>{{(this.cellnumber.innerdata.indexOf(2)!==-1)?2:''}}</span>
+		</div>
+		<div class="area3">
+			<span>{{(this.cellnumber.innerdata.indexOf(3)!==-1)?3:''}}</span>
+		</div>
+
+		<div class="area4">
+			<span>{{(this.cellnumber.innerdata.indexOf(4)!==-1)?4:''}}</span>
+		</div>
+		<div class="area5">
+			<span>{{(this.cellnumber.innerdata.indexOf(5)!==-1)?5:''}}</span>
+		</div>
+		<div class="area6">
+			<span>{{(this.cellnumber.innerdata.indexOf(6)!==-1)?6:''}}</span>
+		</div>
+
+		<div class="area7">
+			<span>{{(this.cellnumber.innerdata.indexOf(7)!==-1)?7:''}}</span>
+		</div>
+		<div class="area8">
+			<span>{{(this.cellnumber.innerdata.indexOf(8)!==-1)?8:''}}</span>
+		</div>
+		<div class="area9">
+			<span>{{(this.cellnumber.innerdata.indexOf(9)!==-1)?9:''}}</span>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'cell',
+		props: {
+			initcellnumber: {
+				type: Object,
+				default: function() {
+					return {};
+				},
+			},
+		},
+		methods: {
+			cellkeyup(event) {
+				console.log(event);
+				switch (event.keyCode) {
+					case 13:	// 按下回车键，确定要填写的数据
+						{
+							if (!this.cellnumber.system && this.cellnumber.innerdata.length === 1) {
+								//如果是用户数据部分，且只有唯一数据，表示用户准备锁定该单元格
+								this.cellnumber.userlocked = true;
+							}
+							break;
+						}
+					case 32:	// 按下了空格键， 清除所填写的数据
+						{
+							if(!this.cellnumber.system){
+								this.cellnumber.innerdata = [];
+								this.cellnumber.userlocked = true;
+							}
+							break;
+						}
+					case 37:	// 左方向键
+						{
+							let cell_id = parseInt(event.target.id);
+							cell_id = cell_id - 1;
+							if(cell_id % 9 === 0){
+								// 在第1列的单元格按下了左方向锓，跳到第9列
+								cell_id = cell_id + 9;
+							}
+							document.getElementById(cell_id).focus();
+							break;
+						}
+					case 38:	// 上方向键
+						{
+							let cell_id = parseInt(event.target.id);
+							cell_id = cell_id - 9;
+							if(cell_id <= 0){
+								// 在第1行的单元各按下了上方向键，跳到第9行
+								cell_id = cell_id + 81;
+							}
+							document.getElementById(cell_id).focus();
+							break;
+						}
+					case 39:	// 右方向键
+						{
+							let cell_id = parseInt(event.target.id);
+							cell_id = cell_id + 1;
+							if(cell_id % 9 === 1){
+								// 在第9列的单元格按下了左方向锓，跳到第1列
+								cell_id = cell_id - 9;
+							}
+							document.getElementById(cell_id).focus();
+							break;
+						}
+					case 40:	//  下方向键
+						{
+							let cell_id = parseInt(event.target.id);
+							cell_id = cell_id + 9;
+							if(cell_id >= 82){
+								// 在第9行的单元各按下了上方向键，跳到第1行
+								cell_id = cell_id - 81;
+							}
+							document.getElementById(cell_id).focus();
+							break;
+						}
+					case 49:
+					case 50:
+					case 51:
+					case 52:
+					case 53:
+					case 54:
+					case 55:
+					case 56:
+					case 57:
+					case 97:
+					case 98:
+					case 99:
+					case 100:
+					case 101:
+					case 102:
+					case 103:
+					case 104:
+					case 105:
+						{
+							if (!this.cellnumber.system) {
+								let x = parseInt(event.key);
+								if (this.cellnumber.innerdata.indexOf(x) === -1) {
+									//添加数值
+									this.cellnumber.innerdata.push(x);
+									this.cellnumber.userlocked = false;
+								} else {
+									//删除数值
+									this.cellnumber.innerdata.splice(this.cellnumber.innerdata.indexOf(x), 1);
+									if(this.cellnumber.innerdata.length===0){
+										this.cellnumber.userlocked = true;
+									}
+								}
+							}
+							break;
+						}
+				}
+			}
+		},
+		computed: {
+			// 如果单元格中的数据有2个及以上，返回true
+			// ismulti:function(){
+			// 	return (this.cellnumber.innerdata.length>1)?true:false;
+			// },
+		},
+		data: function() {
+			return {
+				cellnumber: this.initcellnumber
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	.focused {
+		border: #FF0000 solid 1px
+	}
+
+	.focusedarea1 {
+		border-style: solid;
+		border-width: 1px;
+		border-top-color: red;
+		border-right-color: transparent;
+		border-bottom-color: transparent;
+		border-left-color: red;
+	}
+
+	.focusedarea2 {
+		border-style: solid;
+		border-width: 1px;
+		border-top-color: red;
+		border-right-color: transparent;
+		border-bottom-color: transparent;
+		border-left-color: transparent;
+	}
+
+	.focusedarea3 {
+		border-style: solid;
+		border-width: 1px;
+		border-top-color: red;
+		border-right-color: red;
+		border-bottom-color: transparent;
+		border-left-color: transparent;
+	}
+
+	.focusedarea4 {
+		border-style: solid;
+		border-width: 1px;
+		border-top-color: transparent;
+		border-right-color: transparent;
+		border-bottom-color: transparent;
+		border-left-color: red;
+	}
+
+	.focusedarea6 {
+		border-style: solid;
+		border-width: 1px;
+		border-top-color: transparent;
+		border-right-color: red;
+		border-bottom-color: transparent;
+		border-left-color: transparent;
+	}
+
+	.focusedarea7 {
+		border-style: solid;
+		border-width: 1px;
+		border-top-color: transparent;
+		border-right-color: transparent;
+		border-bottom-color: red;
+		border-left-color: red;
+	}
+
+	.focusedarea8 {
+		border-style: solid;
+		border-width: 1px;
+		border-top-color: transparent;
+		border-right-color: transparent;
+		border-bottom-color: red;
+		border-left-color: transparent;
+	}
+
+	.focusedarea9 {
+		border-style: solid;
+		border-width: 1px;
+		border-top-color: transparent;
+		border-right-color: red;
+		border-bottom-color: red;
+		border-left-color: transparent;
+	}
+
+	.container01 {
+		display: grid;
+		grid-template-columns: repeat(3, 20px);
+		grid-template-rows: repeat(3, 20px);
+		grid-gap: 1px 1px;
+	}
+	
+	.container01:focus{
+		/* border:green solid thin; */
+		outline:#ff0000 solid 3px;
+	}
+
+	.area0 {
+		text-align: center;
+		background-color: gray;
+		font-size: 48px;
+		line-height: 1.25;
+		padding: 0;
+		width: 60px;
+		height: 60px;
+	}
+
+	.areaUserlocked {
+		text-align: center;
+		background-color: darkseagreen;
+		font-size: 48px;
+		line-height: 1.25;
+		padding: 0;
+		width: 60px;
+		height: 60px;
+	}
+
+	.container {
+		display: grid;
+		grid-template-columns: repeat(3, 20px);
+		grid-template-rows: repeat(3, 20px);
+	}
+	
+	.container:focus{
+		/* border:green solid thin; */
+		outline:#ff0000 solid 3px;
+	}
+
+	.area1 {
+		text-align: center;
+		background-color: darkgray;
+		font-size: 18px;
+		line-height: 1.15;
+		width: 20px;
+		height: 20px;
+	}
+
+	.area2 {
+		text-align: center;
+		background-color: wheat;
+		font-size: 18px;
+		line-height: 1.15;
+		width: 20px;
+		height: 20px;
+	}
+
+	.area3 {
+		text-align: center;
+		background-color: darkgray;
+		font-size: 18px;
+		line-height: 1.15;
+		width: 20px;
+		height: 20px;
+	}
+
+	.area4 {
+		text-align: center;
+		background-color: wheat;
+		font-size: 18px;
+		line-height: 1.15;
+		width: 20px;
+		height: 20px;
+	}
+
+	.area5 {
+		text-align: center;
+		background-color: darkgray;
+		font-size: 18px;
+		line-height: 1.15;
+		width: 20px;
+		height: 20px;
+	}
+
+	.area6 {
+		text-align: center;
+		background-color: wheat;
+		font-size: 18px;
+		line-height: 1.15;
+		width: 20px;
+		height: 20px;
+	}
+
+	.area7 {
+		text-align: center;
+		background-color: darkgray;
+		font-size: 18px;
+		line-height: 1.15;
+		width: 20px;
+		height: 20px;
+	}
+
+	.area8 {
+		text-align: center;
+		background-color: wheat;
+		font-size: 18px;
+		line-height: 1.15;
+		width: 20px;
+		height: 20px;
+	}
+
+	.area9 {
+		text-align: center;
+		background-color: darkgray;
+		font-size: 18px;
+		line-height: 1.15;
+		width: 20px;
+		height: 20px;
+	}
+</style>
