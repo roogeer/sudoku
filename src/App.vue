1fdc9d17ba52af146813b5331809327da6f492c0
roogeer <1681991@qq.com>
2020-01-22 14:58:14 +0800

    Sudoku v0.7

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="app">
 		<!--<img alt="Vue logo" src="./assets/logo.png" />-->
-		<HelloWorld msg="Sudoku v0.6" />
+		<HelloWorld msg="Sudoku v0.7" />
 		<div class="hello">
 			<h3>#{{ sudokuid }}</h3>
 		</div>
